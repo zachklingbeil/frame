3b0ae8c39ceba65f200138715db132fc1b87eb29
zachklingbeil <zach@zachklingbeil.com>
2025-11-18 15:34:45 -0800

    update...

--- main.go
+++ main.go
@@ -7,7 +7,6 @@ import (
 	"strconv"
 	"time"
 
-	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 )
 
@@ -26,7 +25,7 @@ type frame struct {
 
 func NewFrame(pathlessUrl, apiURL string) Frame {
 	f := &frame{Router: mux.NewRouter()}
-	f.Router.Use(f.cors(pathlessUrl), f.cache())
+	f.Router.Use(f.middleware(pathlessUrl))
 	f.Forge = NewForge(f.Router, apiURL).(*forge)
 	return f
 }
@@ -65,24 +64,25 @@ func (f *frame) Serve() {
 	}()
 }
 
-func (f *frame) cors(pathlessUrl string) mux.MiddlewareFunc {
+func (f *frame) middleware(pathlessUrl string) mux.MiddlewareFunc {
 	origin := "http://localhost:1000"
 	if pathlessUrl != "" {
 		origin = "https://" + pathlessUrl
 	}
-
-	return handlers.CORS(
-		handlers.AllowedHeaders([]string{"Content-Type", "X-Frame"}),
-		handlers.AllowedOrigins([]string{origin}),
-		handlers.AllowedMethods([]string{"GET", "OPTIONS"}),
-		handlers.ExposedHeaders([]string{"X-Frame", "X-Frames"}),
-	)
-}
-
-func (f *frame) cache() mux.MiddlewareFunc {
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			w.Header().Set("Access-Control-Allow-Origin", origin)
+			w.Header().Set("Access-Control-Allow-Methods", "GET, OPTIONS")
+			w.Header().Set("Access-Control-Allow-Headers", "Content-Type, X-Frame")
+			w.Header().Set("Access-Control-Expose-Headers", "X-Frame, X-Frames")
+
 			w.Header().Set("Cache-Control", "max-age=86400, public")
+			w.Header().Set("Connection", "keep-alive")
+			w.Header().Set("Keep-Alive", "timeout=120, max=100")
+			if r.Method == http.MethodOptions {
+				w.WriteHeader(http.StatusOK)
+				return
+			}
 			next.ServeHTTP(w, r)
 		})
 	}
